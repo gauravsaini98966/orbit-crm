dac15ba32d5d54f2be359315a54b11bcbd746079
Orbit CRM Dev <dev@orbitcrm.local>
2026-07-16 13:38:58 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -9,5 +9,8 @@ const PORT = process.env.PORT || 5000;
 connectDB().finally(() => {
   app.listen(PORT, () => {
     console.log(`[server] Orbit CRM API running on http://localhost:${PORT}`);
+    console.log("MONGO_URL =", process.env.MONGO_URI);
+
   });
 });
+
